29dcbac18428b4377031753d70f14d0261a7ae43
vicfriedman <victoria.m.friedman@gmail.com>
2015-05-01 11:04:24 -0400

    solution

--- flyover.rb
+++ flyover.rb
@@ -1,1 +1,53 @@
-flyover.rb
+def pennsylvania
+  puts "We're flying over Pennsylvania"
+end
+
+def ohio
+  puts "We're flying over Ohio"
+end
+
+def indianna
+  puts "We're flying over Indianna"
+end
+
+def illinois
+  puts "We're flying over Illinois".upcase
+end
+
+def missouri
+  puts "We're flying over Missouri"
+end
+
+def kansas
+  puts "We're flying over Kansas"
+end
+
+def colorado
+  puts "We're flying over Colorado".upcase
+end
+
+def utah
+  puts "We're flying over Utah".reverse
+end
+
+def nevada
+  puts "We're flying over Nevada".reverse
+end
+
+def fasten_seatbelts
+  puts "Please fasten your seatbelts"
+end
+
+def free_to_move_about_the_cabin
+  puts "You are free to move about the cabin"
+end
+
+pennsylvania
+ohio
+indianna
+illinois
+missouri
+kansas
+colorado
+utah
+nevada
